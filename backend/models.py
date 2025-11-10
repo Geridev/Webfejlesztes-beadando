d6f8586fe47dff72d5a6003bace07a2586fa4cce
Lovas Gergő <lovasgergo44@gmail.com>
2025-11-10 19:20:46 +0100

    added: models.py

---
+++ backend/models.py
@@ -0,0 +1,24 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), default='user')
+    appointments = db.relationship('Appointment', backref='user', lazy=True)
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    duration = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    appointments = db.relationship('Appointment', backref='service', lazy=True)
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    datetime = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(20), default='booked')
